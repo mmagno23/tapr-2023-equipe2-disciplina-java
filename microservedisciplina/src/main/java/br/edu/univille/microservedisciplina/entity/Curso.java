7395aebc981213ffe17f7bac9f100e6f96a3432f
mmagno23 <103081924+mmagno23@users.noreply.github.com>
2023-11-17 00:20:18 +0000

    16/11/23

---
+++ microservedisciplina/src/main/java/br/edu/univille/microservedisciplina/entity/Curso.java
@@ -0,0 +1,14 @@
+package br.edu.univille.microservedisciplina.entity;
+
+public class Curso {
+    public int idcurso;
+
+    public int getIdcurso() {
+        return idcurso;
+    }
+
+    public void setIdcurso(int idcurso) {
+        this.idcurso = idcurso;
+    }
+    
+}
