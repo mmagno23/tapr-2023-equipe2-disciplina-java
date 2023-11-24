1fd2bf37173646ec6c045f13e034c9533e0ffb8a
mmagno23 <103081924+mmagno23@users.noreply.github.com>
2023-11-24 01:15:06 +0000

    23/11/2023

--- microservedisciplina/src/main/java/br/edu/univille/microservedisciplina/service/DisciplinaService.java
+++ microservedisciplina/src/main/java/br/edu/univille/microservedisciplina/service/DisciplinaService.java
@@ -1,6 +1,9 @@
 package br.edu.univille.microservedisciplina.service;
 import java.util.List;
 import br.edu.univille.microservedisciplina.entity.Disciplina;
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+
 public interface DisciplinaService {
     public List<Disciplina> getAll();
     public Disciplina save(Disciplina disciplina);
--- microservedisciplina/src/main/java/br/edu/univille/microservedisciplina/service/impl/DisciplinaServiceImpl.java
+++ microservedisciplina/src/main/java/br/edu/univille/microservedisciplina/service/impl/DisciplinaServiceImpl.java
@@ -4,16 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import br.edu.univille.microservedisciplina.entity.Disciplina;
 import br.edu.univille.microservedisciplina.repository.DisciplinaRepository;
 import br.edu.univille.microservedisciplina.service.DisciplinaService;
 
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+
 @Service
 public class DisciplinaServiceImpl implements DisciplinaService{
     @Autowired
     private DisciplinaRepository repository;
+    private DaprClient client = new DaprClientBuilder().build();
+    @Value("${app.component.topic.disciplina}")
+    private String TOPIC_NAME;
+    @Value("${app.component.service}")
+	private String PUBSUB_NAME;
 
     @Override
     public List<Disciplina> getAll() {
@@ -33,20 +42,23 @@ public class DisciplinaServiceImpl implements DisciplinaService{
 
     @Override
     public Disciplina save(Disciplina disciplina) {
-        return repository.save(disciplina);
+        disciplina.setIdDisciplina(null);
+        disciplina = repository.save(disciplina);
+        publicarAtualizacao(disciplina);
+        return disciplina;
     }
-
+    
     @Override
     public Disciplina update(String id, Disciplina disciplina) {
         var buscaDisciplinaAntiga = repository.findById(id);
         if (buscaDisciplinaAntiga.isPresent()){
             var disciplinaAntiga = buscaDisciplinaAntiga.get();
 
-            //Atualizar cada atributo do objeto antigo 
             disciplinaAntiga.setNomeDisciplina(disciplina.getNomeDisciplina());
             disciplinaAntiga.setCargaHoraria(disciplina.getCargaHoraria());            
-            disciplinaAntiga.setSemestre(disciplina.getSemestre());                        
-            return repository.save(disciplinaAntiga);
+            disciplinaAntiga.setSemestre(disciplina.getSemestre());   
+            publicarAtualizacao(disciplinaAntiga);
+            return disciplinaAntiga;
         }
         return null;
     }
@@ -63,6 +75,13 @@ public class DisciplinaServiceImpl implements DisciplinaService{
         }
         return null;
     }
+    private void publicarAtualizacao(Disciplina disciplina){
+        client.publishEvent(
+					PUBSUB_NAME,
+					TOPIC_NAME,
+					disciplina).block();
+    }
+
     
 }
 
